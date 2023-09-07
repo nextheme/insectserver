1ab0db416b79bf135c7a9c51ce20df9077c5075a
Shayan Moradi <shayanmoradi@Shayans-MacBook-Pro.local>
2023-09-08 01:58:37 +0330

    first commit

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 # insectserver
 # insectserver
 # insectserver
+# insectserver
--- database.js
+++ database.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import mongoose, { mongo } from 'mongoose'
 
 const MongoConnect = (uri) => {
